24e0b1a4ea8bbd4d9b12fe3cf18145c94b1c168e
Ibrahim Oubelkas <ibrahim.oubelkas@um5r.ac.ma>
2025-04-04 23:11:54 +0100

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -2,20 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+#define the model
+#the model is a simple CNN with 3 convolutional layers and 2 fully connected layers
+#the model is used to classify the images into 101 classes (0-99 for numbers and 100 for no-number)
 
 class MyModel(nn.Module):
     def __init__(self, num_classes):
         super(MyModel, self).__init__()
         self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = nn.BatchNorm2d(64) #batch normalization to improve training stability and convergence
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
         
-        self.pool = nn.MaxPool2d(2)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
+        self.pool = nn.MaxPool2d(2) #max pooling to reduce the spatial dimensions of the feature maps
+        self.dropout1 = nn.Dropout(0.25) 
+        self.dropout2 = nn.Dropout(0.5) #dropout to prevent overfitting
         
         self.fc1 = nn.Linear(128 * 8 * 8, 512)
         self.fc2 = nn.Linear(512, num_classes + 1)
